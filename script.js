7f781da0b26a9ef8ec35e7c52fdb567d68fd654e
Anna Guegan <anna.guegan@orange.fr>
2024-10-20 10:22:12 +0200

    cool

--- script.js
+++ script.js
@@ -7,80 +7,65 @@ function adjustFlipbookSize() {
     const screenWidth = window.innerWidth;
     let flipbookWidth, flipbookHeight;
 
-    // Adapte la taille en fonction de la largeur de l'écran
     if (screenWidth > 1024) {
-        // Ordinateur : 80% de la largeur de l'écran
         flipbookWidth = screenWidth * 0.7;
-        flipbookHeight = flipbookWidth * 2 / 3; // Ratio 2:3
+        flipbookHeight = flipbookWidth * 2 / 3;
     } else if (screenWidth > 768) {
-        // Tablette : 90% de la largeur de l'écran
         flipbookWidth = screenWidth * 0.9;
-        flipbookHeight = flipbookWidth * 4 / 5; // Ratio 4:5 pour une meilleure visibilité sur tablette
+        flipbookHeight = flipbookWidth * 4 / 5;
     } else {
-        // Téléphone : 100% de la largeur de l'écran
         flipbookWidth = screenWidth * 0.98;
-        flipbookHeight = flipbookWidth * 5/6; // Ratio 1:1.2 pour téléphone (plus grand en hauteur)
+        flipbookHeight = flipbookWidth * 5/6;
     }
 
-    // Applique les nouvelles dimensions au flipbook
     $('#flipbook').turn("size", flipbookWidth, flipbookHeight);
 }
 
-envelope.addEventListener("click", () => {
+// envelope.addEventListener("click", () => {
+window.onload = function() {
     envelope.classList.toggle("flap");
   
-    // Ajoute un délai pour commencer à déplacer l'enveloppe
     setTimeout(() => {
-      // Fait coulisser l'enveloppe vers le haut en douceur
-      envelope.style.transform = "translateY(-100px)"; // Ajuste la valeur selon le déplacement souhaité
-      envelope.style.transition = "transform 1.5s ease-in-out"; // Durée et easing pour un mouvement fluide
+      envelope.style.transform = "translateY(-100px)";
+      envelope.style.transition = "transform 1.5s ease-in-out";
   
-      // Après le mouvement de l'enveloppe, ajoute l'effet de fondu de la lettre
       setTimeout(() => {
         letter.classList.add("fade-out");
 
-  
-        // Utilise un délai pour cacher complètement la lettre après l'effet de fondu
         setTimeout(() => {
-          // Affiche le flipbook
           flipbook.classList.remove("hidden");
           setTimeout(function() {
             flipbook.classList.add("visible");
-          }, 10); // Petit délai pour permettre la transition
+          }, 10);
   
           
           const screenWidth = window.innerWidth;
           let flipbookWidth, flipbookHeight;
       
-          // Adapte la taille en fonction de la largeur de l'écran
           if (screenWidth > 1024) {
-            // Ordinateur : 80% de la largeur de l'écran
             flipbookWidth = screenWidth * 0.7;
-            flipbookHeight = flipbookWidth * 2 / 3; // Ratio 2:3
+            flipbookHeight = flipbookWidth * 2 / 3; 
             } else if (screenWidth > 768) {
-                // Tablette : 90% de la largeur de l'écran
                 flipbookWidth = screenWidth * 0.9;
-                flipbookHeight = flipbookWidth * 4 / 5; // Ratio 4:5 pour une meilleure visibilité sur tablette
+                flipbookHeight = flipbookWidth * 4 / 5;
             } else {
-                // Téléphone : 100% de la largeur de l'écran
                 flipbookWidth = screenWidth * 0.98;
-                flipbookHeight = flipbookWidth * 5/6; // Ratio 1:1.2 pour téléphone (plus grand en hauteur)
+                flipbookHeight = flipbookWidth * 5/6;
             }
 
-          // Initialise le flipbook avec Turn.js
           $('#flipbook').turn({
-            width: flipbookWidth, // Ajuste à 80% de la largeur de l'écran
-            height: flipbookHeight, // Ajuste la hauteur avec un ratio 4:3
-            autoCenter: true
+            width: flipbookWidth,
+            height: flipbookHeight,
+            autoCenter: true,
+            duration: 2000
           });
             setTimeout(function(){
               $('#flipbook').turn("next");
-          }, 3000);  // Adjust the delay time as needed
-        }, 1000); // Délai pour correspondre à la durée de l'effet de fondu
-      }, 1000); // Délai correspondant à la durée du mouvement de l'enveloppe
-    }, 1000); // Légère attente après l'animation de l'enveloppe
-  });
-  
+          }, 2500);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  };
   window.addEventListener('resize', adjustFlipbookSize);
   
   
